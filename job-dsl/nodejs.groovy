69c9ac9a441bf28a5f3ebe63578b18af0da900fd
Sushant <37592117+yagami6@users.noreply.github.com>
2020-02-13 11:48:14 +0530

    Update nodejs.groovy

--- job-dsl/nodejs.groovy
+++ job-dsl/nodejs.groovy
@@ -1,6 +1,6 @@
 job('NodeJS example') {
     scm {
-        git('git://github.com/duorg/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+        git('git://github.com/yagami6/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('admin')
             node / gitConfigEmail('admin@admin.com')
         }
